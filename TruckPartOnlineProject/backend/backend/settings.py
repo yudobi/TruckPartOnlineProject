8ebd479d3c0cbec433c3f82415a8c483321a0e45
jorge <jorgeantonioramirezricardo@gmail.com>
2026-01-28 12:30:44 -0600

    actu static file

--- TruckPartOnlineProject/backend/backend/settings.py
+++ TruckPartOnlineProject/backend/backend/settings.py
@@ -16,7 +16,8 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
